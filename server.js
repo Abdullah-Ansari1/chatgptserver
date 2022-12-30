945eb972ce255361bde0fe54592002536407c90f
AbdullahAnsariskylinx <abd45015@gmail.com>
2022-12-30 16:16:14 +0500

    Revert

--- server.js
+++ server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(process.env.PORT || 5000, () => console.log('AI server started'))
+app.listen(process.env.PORT || 5000, () => console.log('AI server started on http://localhost:5000'))
